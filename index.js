1788e22a85c41e3add5d87f0dcb1d3416415a49e
fayem045 <maniojoannafaye@gmail.com>
2026-04-12 23:46:22 +0800

    Debbuging EmailJs1

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-(function(){
-  emailjs.init("wN3-1Z9GHvFSoD0kZ");
-})();
+// (function(){
+//   emailjs.init("wN3-1Z9GHvFSoD0kZ");
+// })();
 
-document.getElementById("contact-form").addEventListener("submit", function(e) {
-  e.preventDefault();
+// document.getElementById("contact-form").addEventListener("submit", function(e) {
+//   e.preventDefault();
 
-  emailjs.sendForm("service_k4x2f1t", "template_he5kbpe", this)
-    .then(function() {
-  alert("Message sent successfully!");
-  document.getElementById("contact-form").reset();
-    }, function(error) {
-      alert("Failed to send: " + error.text);
-    });
-});
+//   emailjs.sendForm("service_k4x2f1t", "template_he5kbpe", this)
+//     .then(function() {
+//   alert("Message sent successfully!");
+//   document.getElementById("contact-form").reset();
+//     }, function(error) {
+//       alert("Failed to send: " + error.text);
+//     });
+// });
+
+//No function yet, just a placeholder for now
+function sendEmail() {
+  var params = {
+    from_name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+  emailjs.send("service_k4x2f1t", "template_ucext1b", params)
+  .then(function(response) {
+    console.log("SUCCESS", response);
+    alert("Message sent!");
+  })
+  .catch(function(error) {
+    console.error("FAILED", error); 
+    alert("Failed to send");
+  });
+}
