255e63a999b884f9a6843fcd53acb85463e60f6a
fayem045 <maniojoannafaye@gmail.com>
2026-04-13 22:36:48 +0800

    Debbugging JS Day2 Fixed Template

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// (function(){
-//   emailjs.init("wN3-1Z9GHvFSoD0kZ");
-// })();
-
-// document.getElementById("contact-form").addEventListener("submit", function(e) {
-//   e.preventDefault();
-
-//   emailjs.sendForm("service_k4x2f1t", "template_he5kbpe", this)
-//     .then(function() {
-//   alert("Message sent successfully!");
-//   document.getElementById("contact-form").reset();
-//     }, function(error) {
-//       alert("Failed to send: " + error.text);
-//     });
-// });
-
-//No function yet, just a placeholder for now
 
         (function(){
             emailjs.init({
@@ -26,7 +9,7 @@ function sendEmail() {
   let params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
-    number: document.getElementById("phone").value,
+    number: document.getElementById("number").value,
     subject: document.getElementById("subject").value,
     message: document.getElementById("message").value
   };
